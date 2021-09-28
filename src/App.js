203845b869384556268f7ce98aeadbd47d65d1a7
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2021-09-28 09:22:37 +0100

    Wrap App in ApolloClient to link to React, render menu on page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,88 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, createHttpLink } from '@apollo/client'
 
-function App() {
-    return <h1>Menu Test</h1>;
+const client = new ApolloClient({
+    link: new createHttpLink(
+      { uri: 'http://localhost:3000/graphql' }
+    ),
+    fetchOptions: {
+      mode: 'no-cors',
+    },
+    cache: new InMemoryCache()
+  });
+
+const MENU = gql`
+query GetStarters {
+  menu { 
+    starters {
+     id
+     name
+     price
+    }
+    mains {
+      id
+      name
+      price
+    }
+   desserts {
+    id
+    name
+    price
+   }
+  }  
+}
+`
+
+function Menu() {
+  const { data } = useQuery(MENU);
+  console.log('data: ', data?.menu?.starters)
+
+  if (!data) return <p>Loading...</p>;
+
+  return (
+    <>
+      <p>
+        Starters:
+      </p>
+    {data?.menu?.['starters']?.map(({ id, name, price }) => {
+      return (
+        <div key={id}>
+          <span>{name}:</span><span>£{price}</span>
+        </div>
+      )
+    })}
+    <p>
+        Mains:
+    </p>
+    {data?.menu?.['mains']?.map(({ id, name, price }) => {
+      return (
+        <div key={id}>
+          <span>{name}:</span><span>£{price}</span>
+        </div>
+      )
+    })}
+    <p>
+      Desserts:
+    </p>
+    {data?.menu?.['desserts']?.map(({ id, name, price }) => {
+      return (
+        <div key={id}>
+          <span>{name}:</span><span>£{price}</span>
+        </div>
+      )
+    })}
+    </>
+  )
 }
 
-render(<App />, document.getElementById('root'));
+function App() {
+    return (
+      <>
+        <h1>Menu Test</h1>
+        <Menu />
+      </>
+    );
+}
+
+render(<ApolloProvider client={client} ><App /></ApolloProvider>, document.getElementById('root'));
--- src/server/static/gql-schema.js
+++ src/server/static/gql-schema.js
@@ -1,44 +1,17 @@
 module.exports = `
-    """
-    The Dish represents the single item on the menu
-    """
-    type Dish {
-        """
-        The unique ID of the dish
-        """
-        id: ID!
-        """
-        The name of the dish
-        """
-        name: String!
-        """
-        The price of the dish
-        """
-        price: Float!
-    }
+  type Dish {
+      id: ID!
+      name: String!
+      price: Float!
+  }
 
-    """
-    The Menu represents the categorized dishes available in the restaurant
-    """
-    type Menu {
-        """
-        The starter category dishes
-        """
-        starters: [Dish!]!
-        """
-        The main category dishes
-        """
-        mains: [Dish!]!
-        """
-        The dessert category dishes
-        """
-        desserts: [Dish!]!
-    }
+  type Menu {
+      starters: [Dish!]!
+      mains: [Dish!]!
+      desserts: [Dish!]!
+  }
 
-    type Query {
-        """
-        The query to get the restaurant menu.
-        """
-        menu: Menu!
-    }
+  type Query {
+      menu: Menu!
+  }
 `;
