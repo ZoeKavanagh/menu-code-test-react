27c94662e7e225e4ae4027374be0f9f5eeb2a7e1
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2021-09-29 14:00:52 +0100

    refactor menu to add conditional logic for rules

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
-import { Course } from '../Course';
-import { Total } from '../Total'
+import React, { useState, useEffect } from 'react';
 
-export const Menu = ({ menu, userId }) => {
-  const [user1Selection, setUser1Selection] = useState({ Starters: { name: '', price: 0 }, Mains: { name: '', price: 0 }, Desserts: { name: '', price: 0 }})
-  const [user2Selection, setUser2Selection] = useState({ Starters: { name: '', price: 0 }, Mains: { name: '', price: 0 }, Desserts: { name: '', price: 0 }})
-  
+import { Course } from '../Course';
+import { Total } from '../Total';
+import { ErrorMessage } from '../ErrorMessage';
+import { Button } from '../Button';
+import { OrderSummary } from '../OrderSummary';
+
+import { getErrorMessage } from '../../utils';
+import { STARTERS, MAINS, DESSERTS } from '../../constants';
+
+export const Menu = ({ menu, isUser1 }) => {
+  //TODO: store user selections with ApolloClient
+  const [user1Selection, setUser1Selection] = useState({ starters: { name: undefined, price: 0 }, mains: { name: undefined, price: 0 }, desserts: { name: undefined, price: 0 }})
+  const [user2Selection, setUser2Selection] = useState({ starters: { name: undefined, price: 0 }, mains: { name: undefined, price: 0 }, desserts: { name: undefined, price: 0 }})
+  const [showOrderSummary, setShowOrderSummary] = useState(false)
+
+  //TODO: store error messages with ApolloClient and allow for more than error to be showed at once
+  const [error, setError] = useState(undefined)
+
   const { starters, mains, desserts } = menu;
 
-  const onUserMenuSelect = (courseType, id, name, price) => {
-    userId === 1 ?
+  const updateUser1MenuSelection = (courseType, id, name, price) => {
       setUser1Selection(prevState => ({
         ...prevState,
         [courseType]: { id, name, price },
       }))
-    : 
+      getErrorMessage(user1Selection, user2Selection, setError)
+  }
+
+  const updateUser2MenuSelection = ( courseType, id, name, price) => {
       setUser2Selection(prevState => ({
-      ...prevState,
-      [courseType]: { id, name, price },
-    }))
+        ...prevState,
+        [courseType]: { id, name, price },
+      }))
+      getErrorMessage(user1Selection, user2Selection, setError)
   }
 
   const getAmount = () => {
-    const { Starters, Mains, Desserts } = user1Selection
-    // const { Starters, Mains, Desserts } = user2Selection
-    
-    return  Starters.price + Mains.price + Desserts.price
+    const totalUser1Menu = user1Selection.starters.price + user1Selection.mains.price + user1Selection.desserts.price
+    const totalUser2Menu = user2Selection.starters.price + user2Selection.mains.price + user2Selection.desserts.price
+    return  totalUser1Menu + totalUser2Menu
   }
 
-  console.log('user1Selection: ', user1Selection)
+  useEffect(() => {
+   getErrorMessage(user1Selection, user2Selection, setError)
+  }, [user1Selection, user2Selection])
 
   return (
     <>
-      <Course courseType={'Starters'} courseDetails={starters} onCourseSelect={onUserMenuSelect} />
-      <Course courseType={'Mains'} courseDetails={mains} onCourseSelect={onUserMenuSelect} />
-      <Course courseType={'Desserts'} courseDetails={desserts} onCourseSelect={onUserMenuSelect} />
+      <Course courseType={STARTERS} courseDetails={starters} onCourseSelect={isUser1 ? updateUser1MenuSelection : updateUser2MenuSelection} />
+      <Course courseType={MAINS} courseDetails={mains} onCourseSelect={isUser1 ? updateUser1MenuSelection : updateUser2MenuSelection} />
+      <Course courseType={DESSERTS} courseDetails={desserts} onCourseSelect={isUser1 ? updateUser1MenuSelection : updateUser2MenuSelection} />
+      <Button label={'Save Selection'} onClick={() => setShowOrderSummary(true)}/>
+      {showOrderSummary && <OrderSummary userOneSelection={user1Selection} userTwoSelection={user2Selection} />}
       <Total amount={getAmount()} />
+      {error && <ErrorMessage errorMessage={error} />}
     </>
   );
 }
