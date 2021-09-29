df85e36c631239e2e0e95deefcc0d18aeefdd45f
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2021-09-29 14:00:01 +0100

    update SelectUser to use reactiveVariable

--- src/components/SelectUser/index.jsx
+++ src/components/SelectUser/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useReactiveVar } from '@apollo/client';
 
-import { Checkbox } from '../Checkbox'
+import { Checkbox } from '../Checkbox';
+import { activeUserIdVar } from '../../cache';
 
-export const SelectUser = ({ onSelectUser }) => {
+
+export const SelectUser = () => {
+  const userId = useReactiveVar(activeUserIdVar)
+  console.log('userId', userId)
   return (
     <>
       <div>Select Dinner One</div>
-      <Checkbox onSelection={() => onSelectUser(1)} />
+      <Checkbox
+        onSelection={() => activeUserIdVar(1)}
+        isChecked={userId === 1} />
       <div>Select Dinner Two</div>
-      <Checkbox onSelection={() => onSelectUser(2)} />
+      <Checkbox
+        onSelection={() => activeUserIdVar(2)}
+        isChecked={userId === 2}
+      />
     </>
   )
 }
